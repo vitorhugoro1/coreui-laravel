5b90cba7259bd777819cc0251429af58c0c5d7e8
Piotr <pio.skro@gmail.com>
2018-01-16 21:26:04 +0100

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,16 @@
 let mix = require('laravel-mix');
-
+let ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
+mix.webpackConfig( {
+    plugins: [
+        new ImageminPlugin( {
+//            disable: process.env.NODE_ENV !== 'production', // Disable during development
+            pngquant: {
+                quality: '95-100',
+            },
+            test: /\.(jpe?g|png|gif|svg)$/i,
+        } ),
+    ],
+} )
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +22,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css/style.css')
+	.copy( 'node_modules/@coreui/ajax/Static_Full_Project_GULP/src/img', 'public/img', false )
+	.options({ processCssUrls: false })
+	.browserSync('http://coreui-laravel.test');
+	// .js('resources/assets/js/app.js', 'public/js')
