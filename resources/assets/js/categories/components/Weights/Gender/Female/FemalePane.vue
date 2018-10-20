eb6fad0a1c9183194bc12ab4a3fa8f914ea40f2a
Vitor Hugo <vitorhugo.ro10@gmail.com>
2018-10-20 19:18:51 -0300

    Ajustes no Vue e public

---
+++ resources/assets/js/categories/components/Weights/Gender/Female/FemalePane.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tab-pane show active" id="male" role="tabpanel">
+        <button type="button" class="pull-right my-2 btn btn-success">Add</button>
+        <male-table></male-table>
+    </div>
+</template>
+
+<script>
+import MaleTable from './MaleTable'
+export default {
+  name: 'male-pane',
+  components: {
+    MaleTable
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ resources/assets/js/categories/components/Weights/Gender/Female/FemaleTable.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th><input type="checkbox"></th>
+                <th>Initial</th>
+                <th>Max</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(male, key) in males" :key="key">
+                <td><input type="checkbox"></td>
+                <td>{{ male.initial }}</td>
+                <td>{{ male.max }}</td>
+                <td>
+                    <button type="button" class="btn btn-primary">Edit</button>
+                    <button type="button" class="btn btn-danger">Remove</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'male-table'
+  }
+</script>
+
+<style scoped></style>
