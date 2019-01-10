e3f1cf629225c5de77f96c42f750d4d0dac4bd7a
Vitor Hugo <vitorhugo.ro10@gmail.com>
2019-01-09 22:06:03 -0200

    Ajustes

--- resources/assets/js/categories/components/Weights/Ageing/Gender/Male/MalePane.vue
+++ resources/assets/js/categories/components/Weights/Ageing/Gender/Male/MalePane.vue
@@ -1,6 +1,20 @@
 <script>
 export default {
-  name: "male-pane"
+  name: "male-pane",
+  data: () => ({
+    ageings: [
+      {
+        name: 'Junior',
+        key: 'junior',
+        data: [
+          {
+            min: 0,
+            max: 10
+          }
+        ]
+      }
+    ]
+  })
 };
 </script>
 
@@ -10,11 +24,38 @@ export default {
       <div class="card">
         <div class="card-header">
           <h5 class="mb-0">
-            <a class="btn btn-link" data-toggle="collapse" data-target="#junior">Ageing Title</a>
+            <a class="btn btn-link" data-toggle="collapse" data-target="#junior">
+              Ageing Title
+            </a>
           </h5>
         </div>
         <div class="collapse show" id="junior" data-parent="#malePaneAccordion">
-          <div class="card-body">tabela</div>
+          <div class="card card-body">
+            <table class="table table-striped">
+              <thead>
+                <tr>#</tr>
+                <tr>Initial</tr>
+                <tr>Max</tr>
+                <tr></tr>
+              </thead>
+              <tbody>
+                <tr v-for="(junior, id) in juniors" :key="id">
+                  <td><input type="checkbox"></td>
+                  <td>{{ junior.min }}</td>
+                  <td>{{ junior.max }}</td>
+                  <td>
+                      <button type="button" class="btn btn-primary" @click="edit(id)" data-toggle="modal" data-target="#junior-edit">Edit</button>
+                      <button type="button" class="btn btn-danger" @click="remove(id)">Remove</button>
+                  </td>
+                </tr>
+                <tr v-if="juniors.length === 0">
+                  <td colspan="4">
+                    <span class="text-bold">Not weights added</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
